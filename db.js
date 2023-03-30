1e60459281816498ee374d2f601107f525fd0542
nickgitgrid <102295766+nickgitgrid@users.noreply.github.com>
2023-03-30 06:33:21 -0800

    commit

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ module.exports = {
      connectToDb: (cb) => {
         // mongoose.connect('mongodb+srv://tutnetwork:Thunder@cluster0.39qnub8.mongodb.net/bookstore?retryWrites=true&w=majority')
         // MongoClient.connect('mongodb://localhost:27017/bookstore')
-        MongoClient.connect('mongodb+srv://ogfox:Maelsav100@cluster0.5lse6bh.mongodb.net/?retryWrites=true&w=majority')
+        MongoClient.connect('mongodb+srv://mikedraxler:Rji3HDQ4L2kHSTT@cluster0.purhky7.mongodb.net/?retryWrites=true&w=majority')
         .then((client) => {
             dbConnection =  client.db()
             return cb()
